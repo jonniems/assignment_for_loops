8389ee88543f15a0ca42fba02fc3c43e1cbaf61d
Jonnie <jonnie.muis@gmail.com>
2020-11-03 10:04:03 +0100

    Add files via upload

---
+++ 08_for_loops/grade-assigner.js
@@ -0,0 +1,16 @@
+for (let i = 60; i <= 100; i++) {
+    score = i;
+    function assignGrade(score) {
+        if (score > 90) {
+            return 'A';
+        } else if (score > 80) {
+            return 'B';
+        } else if (score > 70) {
+            return 'C';
+        } else if (score > 65) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    } console.log('Voor ' + i + ' punten, krijg je een ' + assignGrade(score));
+};
